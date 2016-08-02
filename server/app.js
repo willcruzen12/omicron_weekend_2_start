a4e604a49fc1652a55f926d66f0963a27c537727
Kris Szafranski <kdszafranski@gmail.com>
2016-08-02 16:05:56 -0500

    began updating for Omicron

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 var path = require("path");
-var data = require("./public/data/mu");
+var data = require("./public/data/omicron");
 
 app.set("port", (process.env.PORT || 5000));
 
